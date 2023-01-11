7702239f32c75dc5ffa328237d73c0321f5e079a
JoaoGMagri <jgmagri@hotmail.com>
2023-01-11 10:00:47 -0300

    feat: routes follow and unfollow

--- src/routers/users.routes.js
+++ src/routers/users.routes.js
@@ -7,5 +7,7 @@ import { authorization } from "../middleware/authorization.middleware.js";
 const routes = express.Router();
 
 routes.get("/users/:id", authorization, usersControllers.viewAllPostsByUser);
+routes.post("/follow", authorization, usersControllers.follow);
+routes.delete("/unfollow/:id", authorization, usersControllers.unfollow);
 
 export default routes;
